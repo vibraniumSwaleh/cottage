c3e2a1b5a5d397284fd1b840d993702ec75195d9
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-31 23:26:10 +0000

    complete sidebar style,layout

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -7,12 +7,16 @@ const StyledLogo = styled.div`
 const Img = styled.img`
   height: 9.6rem;
   width: auto;
+  border-radius: 100%;
 `;
 
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <>
+        <Img src='/cottage.webp' alt='Logo' />
+        <p>COTTAGE</p>
+      </>
     </StyledLogo>
   );
 }
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
@@ -8,7 +13,12 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
-  return <StyledSidebar>SIDEBAR</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
