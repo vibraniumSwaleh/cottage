ddd9fb1113a5d5201daf012eed8c40b41ac60e21
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-31 22:38:45 +0000

    style layout for  AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import NewUsers from './pages/Users';
@@ -7,23 +9,30 @@ import Cabins from './pages/Cabins';
 import Account from './pages/Account';
 import Settings from './pages/Settings';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
-    <BrowserRouter
-      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    >
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='login' element={<Login />} />
-        <Route path='users' element={<NewUsers />} />
-        <Route path='bookings' element={<Bookings />} />
-        <Route path='cabins' element={<Cabins />} />
-        <Route path='account' element={<Account />} />
-        <Route path='settings' element={<Settings />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='users' element={<NewUsers />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='account' element={<Account />} />
+            <Route path='settings' element={<Settings />} />
+          </Route>
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
