4e6e8cf13e67355d17f818bd52aad31f11dabfd3
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-03-01 15:16:00 +0000

    add event listener to close modal on click outside model component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -23,21 +23,4 @@ function AddCabin() {
   );
 }
 
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}>
-//         {`${isOpenModal ? 'Hide Cabin Form' : 'Add Cabin From'} `}
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
-
 export default AddCabin;
